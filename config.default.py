ac03d813d0908370c05bedbc35b782590be7a4d9
Sergey Popinevskiy <gurza000@gmail.com>
2016-02-10 19:28:45 +0300

    Actualize config sample

--- config.default.py
+++ config.default.py
@@ -1,3 +1,12 @@
 #coding: utf-8
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 CSRF_ENABLED = True
 SECRET_KEY = 'my-secret-secret-key'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+APP_REDIRECT_URL = 'https://www.facebook.com'
