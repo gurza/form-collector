0fd88a5f3616dd02a510fe7d82d84a7199840378
Sergey Popinevskiy <gurza@InTheAir.local>
2016-02-10 14:54:53 +0300

    Play time with data

--- index.py
+++ index.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 #coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, Response, render_template, redirect
+from flask.ext.sqlalchemy import SQLAlchemy
+import simplejson
 import config
 from forms import LoginForm
 
 
 app = Flask('index')
 app.config.from_object(config)
+db = SQLAlchemy(app)
+
+
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(64), index=True, unique=False)
+    password = db.Column(db.String(64), index=False, unique=False)
+
+    def __repr__(self):
+        return '<Login %r:%r>' % (self.login, self.password)
 
 
 @app.route('/api/v1/ping', methods = ['GET'])
@@ -14,9 +26,41 @@ def ping():
     return '', 200
 
 
-@app.route('/', methods = ['GET'])
+def jsonify(hash):
+
+    res = simplejson.dumps(hash, ensure_ascii=False)
+
+
+@app.route('/api/v1/logins', methods = ['GET'])
+def logins_list():
+    logins = Login.query.all()
+
+    res = []
+    for l in logins:
+        res.append({
+            'login': l.login,
+            'password': l.password,
+        })
+
+    return Response(
+        simplejson.dumps(res, ensure_ascii=False),
+        mimetype = 'application/json'
+    )
+
+
+@app.route('/', methods = ['GET', 'POST'])
 def form_ctrl():
     form = LoginForm()
+    if form.validate_on_submit():
+        print form.login.data
+        print form.password.data
+        login = Login(
+                    login = form.login.data,
+                    password = form.password.data
+        )
+        db.session.add(login)
+        db.session.commit()
+        return redirect('/')
     return render_template('form.html', form=form)
 
 
